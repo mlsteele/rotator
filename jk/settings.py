83310d3893f21fcb75f009dab056567c40647823
Miles Steele <miles@milessteele.com>
2013-07-05 00:27:26 -0400

    test hook commit

--- jk/settings.py
+++ jk/settings.py
@@ -23,6 +23,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+# ALLOWED_HOSTS = ['jesskenney.com']
 ALLOWED_HOSTS = []
 
 # Local time zone for this installation. Choices can be found here:
