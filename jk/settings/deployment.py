187bd02ca467bc89d4d827ca89180162760d2e88
Miles Steele <miles@milessteele.com>
2013-07-07 01:59:15 -0400

    add jesskenney.com to ALLOWED_HOSTS

--- jk/settings/deployment.py
+++ jk/settings/deployment.py
@@ -5,5 +5,4 @@ TEMPLATE_DEBUG = False
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-# ALLOWED_HOSTS = ['jesskenney.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jesskenney.com']
